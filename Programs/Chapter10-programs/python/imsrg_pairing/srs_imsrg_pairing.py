84df6417f68168f179944ef7f846198b2f5df02b
Ragnar Stroberg <ragnarstroberg@gmail.com>
2018-06-18 18:16:30 -0700

    Added the beginnings of IMSRG 3 to srs_imsrg_pairing.py

--- Programs/Chapter10-programs/python/imsrg_pairing/srs_imsrg_pairing.py
+++ Programs/Chapter10-programs/python/imsrg_pairing/srs_imsrg_pairing.py
@@ -46,7 +46,7 @@ def construct_basis_2B(holes, particles):
 
   return basis
 
-
+# how is this different from construct_basis_2B ???
 def construct_basis_ph2B(holes, particles):
   basis = []
   for i in holes:
@@ -79,6 +79,48 @@ def construct_index_2B(bas2B):
   return index
 
 
+#SRS
+#SRS  3-body stuff
+#SRS
+
+# Doing this about as dumb as can be
+
+def construct_basis_3B(holes, particles):
+  basis = []
+  for i in holes:
+    for j in holes:
+      for k in holes:
+        basis.append((i, j, k))
+
+  for i in holes:
+    for j in holes: 
+      for a in particles:
+        basis.append((i, j, a))
+        basis.append((i, a, j))
+        basis.append((a, i, j))
+
+  for i in holes:
+    for a in particles:
+      for b in particles:
+        basis.append((i, a, b))
+        basis.append((a, i, b))
+        basis.append((a, b, i))
+
+  for a in particles:
+    for b in particles:
+      for c in particles:
+        basis.append((a, b, c))
+
+  return basis
+
+
+#SRS this is identical to construct_index_2B
+def construct_index_3B(bas3B):
+  index = { }
+  for i, state in enumerate(bas3B):
+    index[state] = i
+
+  return index
 
 #-----------------------------------------------------------------------------------
 # transform matrices to particle-hole representation
@@ -136,6 +178,26 @@ def calc_Gammaod_norm(Gamma, user_data):
           norm += Gamma[idx2B[(a,b)],idx2B[(i,j)]]**2 + Gamma[idx2B[(i,j)],idx2B[(a,b)]]**2
 
   return np.sqrt(norm)
+
+
+
+#SRS
+def calc_Wod_norm(W, user_data):
+  particles = user_data["particles"]
+  holes     = user_data["holes"]
+  idx3B     = user_data["idx3B"]
+
+  norm = 0.0
+  for a in particles:    
+    for b in particles:
+      for c in particles:
+        for i in holes:
+          for j in holes:
+            for k in holes:
+              norm += W[idx3B[(a,b,c)],idx3B[(i,j,k)]]**2 + W[idx3B[(i,j,k)],idx3B[(a,b,c)]]**2
+
+  return np.sqrt(norm)
+
 
 #-----------------------------------------------------------------------------------
 # occupation number matrices
@@ -502,6 +564,93 @@ def eta_wegner(f, Gamma, user_data):
   return eta1B, eta2B
 
 
+#-----------------------------------------------------------------
+
+#SRS   3B  generator
+#SRS
+def eta_white_3B(f, Gamma, W, user_data):
+  dim1B     = user_data["dim1B"]
+  particles = user_data["particles"]
+  holes     = user_data["holes"]
+  idx2B     = user_data["idx2B"]
+  idx3B     = user_data["idx3B"]
+
+  # one-body part of the generator
+  eta1B  = np.zeros_like(f)
+
+  for a in particles:
+    for i in holes:
+      denom = f[a,a] - f[i,i] + Gamma[idx2B[(a,i)], idx2B[(a,i)]]
+      val = f[a,i]/denom
+      eta1B[a, i] =  val
+      eta1B[i, a] = -val 
+
+  # two-body part of the generator
+  eta2B = np.zeros_like(Gamma)
+
+  for a in particles:
+    for b in particles:
+      for i in holes:
+        for j in holes:
+          denom = ( 
+            f[a,a] + f[b,b] - f[i,i] - f[j,j]  
+            + Gamma[idx2B[(a,b)],idx2B[(a,b)]] 
+            + Gamma[idx2B[(i,j)],idx2B[(i,j)]]
+            - Gamma[idx2B[(a,i)],idx2B[(a,i)]] 
+            - Gamma[idx2B[(a,j)],idx2B[(a,j)]] 
+            - Gamma[idx2B[(b,i)],idx2B[(b,i)]] 
+            - Gamma[idx2B[(b,j)],idx2B[(b,j)]] 
+          )
+
+          val = Gamma[idx2B[(a,b)], idx2B[(i,j)]] / denom
+
+          eta2B[idx2B[(a,b)],idx2B[(i,j)]] = val
+          eta2B[idx2B[(i,j)],idx2B[(a,b)]] = -val
+
+  #SRS three-body part of the generator
+  #SRS note that I negelect 3b contributions to the denominator
+  eta3B = np.zeros_like(W)
+
+  for a in particles:
+    for b in particles:
+      for c in particles:
+        for i in holes:
+          for j in holes:
+            for k in holes:
+              denom = ( 
+                f[a,a] + f[b,b] +f[c,c] - f[i,i] - f[j,j] -f[k,k] 
+                + Gamma[idx2B[(a,b)],idx2B[(a,b)]] 
+                + Gamma[idx2B[(a,c)],idx2B[(a,c)]] 
+                + Gamma[idx2B[(b,c)],idx2B[(b,c)]] 
+                + Gamma[idx2B[(i,j)],idx2B[(i,j)]]
+                + Gamma[idx2B[(i,k)],idx2B[(i,k)]]
+                + Gamma[idx2B[(j,k)],idx2B[(j,k)]]
+                - Gamma[idx2B[(a,i)],idx2B[(a,i)]] 
+                - Gamma[idx2B[(a,j)],idx2B[(a,j)]] 
+                - Gamma[idx2B[(a,k)],idx2B[(a,k)]] 
+                - Gamma[idx2B[(b,i)],idx2B[(b,i)]] 
+                - Gamma[idx2B[(b,j)],idx2B[(b,j)]] 
+                - Gamma[idx2B[(b,k)],idx2B[(b,k)]] 
+                - Gamma[idx2B[(c,i)],idx2B[(c,i)]] 
+                - Gamma[idx2B[(c,j)],idx2B[(c,j)]] 
+                - Gamma[idx2B[(c,k)],idx2B[(c,k)]] 
+              )
+    
+              val = W[idx3B[(a,b,c)], idx3B[(i,j,k)]] / denom
+    
+              eta3B[idx3B[(a,b,c)],idx3B[(i,j,k)]] = val
+              eta3B[idx3B[(i,j,k)],idx3B[(a,b,c)]] = -val
+
+
+
+  return eta1B, eta2B, eta3B
+
+
+
+
+
+
+
 #-----------------------------------------------------------------------------------
 # derivatives 
 #-----------------------------------------------------------------------------------
@@ -630,11 +779,125 @@ def flow_imsrg2(eta1B, eta2B, f, Gamma, user_data):
 
   return dE, df, dGamma
 
+#-------------------------------------------------------------------------
+
+#SRS 
+def flow_imsrg3(eta1B, eta2B, eta3B, f, Gamma, W, user_data):
+  dim1B     = user_data["dim1B"]
+  holes     = user_data["holes"]
+  particles = user_data["particles"]
+  bas2B     = user_data["bas2B"]
+  bas3B     = user_data["bas3B"]
+  idx2B     = user_data["idx2B"]
+  basph2B   = user_data["basph2B"]
+  idxph2B   = user_data["idxph2B"]
+  idx3B     = user_data["idx3B"]
+  occB_2B   = user_data["occB_2B"]
+  occC_2B   = user_data["occC_2B"]
+  occphA_2B = user_data["occphA_2B"]
+
+  #SRS Reuse the imsrg(2) stuff
+  dE, df, dGamma = flow_imsrg2(eta1B, eta2B, f, Gamma, user_data)
+  dW = np.zeros_like(W)
+
+  dE3 = 0.
+  #SRS zero body piece
+  for a in particles:
+    for b in particles:
+      for c in particles:
+        for i in holes:
+          for j in holes:
+            for k in holes:
+              dE3 += 1./18 * eta3B[idx3B[(i,j,k)], idx3B[(a,b,c)]] * W[idx3B[(a,b,c)], idx3B[(i,j,k)]]
+  dE += dE3
+  print "dE3 = ",dE3
+#
+#
+#  #SRS one body piece
+#  #SRS [3,3]->1
+#  # This can be written as a matrix multiplication with an occupation matrix thrown in there
+#  for p in range(dim1B):
+#    for q in range(dim1B):
+#      for i in holes:
+#        for j in holes:
+#          for a in particles:
+#            for b in particles:
+#              for c in particles:
+#                df[p,q] += 1./12 * eta3B[idx3B[(p,i,j)], idx3B[(a,b,c)]] *     W[idx3B[(a,b,c)], idx3B[(q,i,j)]]
+#                df[p,q] -= 1./12 *     W[idx3B[(p,i,j)], idx3B[(a,b,c)]] * eta3B[idx3B[(a,b,c)], idx3B[(q,i,j)]]
+#              for k in holes:
+#                df[p,q] += 1./12 * eta3B[idx3B[(p,a,b)], idx3B[(i,j,k)]] *     W[idx3B[(i,j,k)], idx3B[(q,a,b)]]
+#                df[p,q] -= 1./12 *     W[idx3B[(p,a,b)], idx3B[(i,j,k)]] * eta3B[idx3B[(i,j,k)], idx3B[(q,a,b)]]
+#
+#
+#  #SRS [2,3]->1 and [3,2]->1
+#  for p in range(dim1B):
+#    for q in range(dim1B):
+#      for r in range(dim1B):
+#        for s in range(dim1B):
+#          for i in range(holes):
+#            for j in range(holes):
+#              df[p,q] += 0.25 * eta2B[idx2B[(i,j)], idx2B[(r,s)]] * (     W[idx3B[(r,s,p)], idx3B[(i,j,q)]] +     W[idx3b[(i,j,p)], idx3b[(r,s,q)]]  )
+#              df[p,q] -= 0.25 * Gamma[idx2B[(i,j)], idx2B[(r,s)]] * ( eta3B[idx3B[(r,s,p)], idx3B[(i,j,q)]] - eta3B[idx3b[(i,j,p)], idx3b[(r,s,q)]]  )
+
+
+  #SRS two body piece (gulp)
+  #SRS [3,3]->2
+ 
+
+  #SRS [2,3]->2 and [3,2]->2
+
+  #SRS [1,3]->2 and [3,1]->2
+
+
+
+
+  #SRS three body piece (double gulp)
+  #SRS [2,2]->3
+  for bra,(p,q,r) in enumerate(bas3B):
+    pq = idx2B[(p,q)]
+    rq = idx2B[(r,q)]
+    pr = idx2B[(p,r)]
+    for ket,(s,t,u) in enumerate(bas3B):
+      if ket<bra: continue
+      tu = idx2B[(t,u)]
+      su = idx2B[(s,u)]
+      ts = idx2B[(t,s)]
+#      print 5*' ',bra,(p,q,r),' ' ,ket,(s,t,u)
+      val = 0.
+      for v in range(dim1B):
+        sv = idx2B[(s,v)]
+        tv = idx2B[(t,v)]
+        uv = idx2B[(u,v)]
+        vp = idx2B[(v,p)]
+        vq = idx2B[(v,q)]
+        vr = idx2B[(v,r)]
+        val +=(eta2B[pq, sv ] * Gamma[vr, tu ] - Gamma[pq, sv ] * eta2B[vr, tu ]
+          - eta2B[rq, sv ] * Gamma[vp, tu ] - Gamma[rq, sv ] * eta2B[vp, tu ]
+          - eta2B[pr, sv ] * Gamma[vq, tu ] - Gamma[pr, sv ] * eta2B[vq, tu ]
+          - eta2B[pq, tv ] * Gamma[vr, su ] - Gamma[pq, tv ] * eta2B[vr, su ]
+          + eta2B[rq, tv ] * Gamma[vp, su ] - Gamma[rq, tv ] * eta2B[vp, su ]
+          + eta2B[pr, tv ] * Gamma[vq, su ] - Gamma[pr, tv ] * eta2B[vq, su ]
+          - eta2B[pq, uv ] * Gamma[vr, ts ] - Gamma[pq, uv ] * eta2B[vr, ts ]
+          + eta2B[rq, uv ] * Gamma[vp, ts ] - Gamma[rq, uv ] * eta2B[vp, ts ]
+          + eta2B[pr, uv ] * Gamma[vq, ts ] - Gamma[pr, uv ] * eta2B[vq, ts ] )
+      dW[bra,ket] += val
+      if bra != ket:
+        dW[ket,bra] += val
+
+  print 'max dW:', np.amax(dW)
+  #SRS [3,3]->3
+
+  #SRS [2,3]->3 and [3,2]->3
+
+  #SRS [1,3]->3 and [3,1]->3
+
+  return dE, df, dGamma, dW
 
 #-----------------------------------------------------------------------------------
 # derivative wrapper
 #-----------------------------------------------------------------------------------
-def get_operator_from_y(y, dim1B, dim2B):
+def get_operator_from_y(y, dim1B, dim2B, dim3B):
   
   # reshape the solution vector into 0B, 1B, 2B pieces
   ptr = 0
@@ -646,13 +909,17 @@ def get_operator_from_y(y, dim1B, dim2B):
   ptr += dim1B*dim1B
   two_body = reshape(y[ptr:ptr+dim2B*dim2B], (dim2B, dim2B))
 
-  return zero_body,one_body,two_body
+  ptr += dim2B*dim2B
+  three_body = reshape(y[ptr:ptr+dim3B*dim3B], (dim3B, dim3B))
+
+  return zero_body,one_body,two_body,three_body
 
 
 def derivative_wrapper(t, y, user_data):
 
   dim1B = user_data["dim1B"]
   dim2B = dim1B*dim1B
+  dim3B = dim1B*dim1B*dim1B
 
 
   holes     = user_data["holes"]
@@ -662,6 +929,7 @@ def derivative_wrapper(t, y, user_data):
   basph2B   = user_data["basph2B"]
   idx2B     = user_data["idx2B"]
   idxph2B   = user_data["idxph2B"]
+  idx3B     = user_data["idx3B"]
   occA_2B   = user_data["occA_2B"]
   occB_2B   = user_data["occB_2B"]
   occC_2B   = user_data["occC_2B"]
@@ -670,17 +938,23 @@ def derivative_wrapper(t, y, user_data):
   calc_rhs  = user_data["calc_rhs"]
 
   # extract operator pieces from solution vector
-  E, f, Gamma = get_operator_from_y(y, dim1B, dim2B)
+#  E, f, Gamma, W = get_operator_from_y(y, dim1B, dim2B)
+  E, f, Gamma, W = get_operator_from_y(y, dim1B, dim2B, dim3B)
 
 
   # calculate the generator
-  eta1B, eta2B = calc_eta(f, Gamma, user_data)
+#  eta1B, eta2B = calc_eta(f, Gamma, user_data)
+  eta1B, eta2B, eta3B = calc_eta(f, Gamma, W, user_data)
 
   # calculate the right-hand side
-  dE, df, dGamma = calc_rhs(eta1B, eta2B, f, Gamma, user_data)
+#  dE, df, dGamma = calc_rhs(eta1B, eta2B, f, Gamma, user_data)
+  dE, df, dGamma, dW = calc_rhs(eta1B, eta2B, eta3B, f, Gamma, W, user_data)
+
+#  dW = np.zeros_like(W)
 
   # convert derivatives into linear array
-  dy   = np.append([dE], np.append(reshape(df, -1), reshape(dGamma, -1)))
+#  dy   = np.append([dE], np.append(reshape(df, -1), reshape(dGamma, -1)))
+  dy   = np.concatenate( ([dE], reshape(df,-1), reshape(dGamma,-1), reshape(dW,-1) ) )
 
   # share data
   user_data["dE"] = dE
@@ -822,7 +1096,7 @@ def main():
   particles  = 4
 
   # setup shared data
-  dim1B     = 16
+  dim1B     = 8
 
   # this defines the reference state
   # 1st state
@@ -841,10 +1115,16 @@ def main():
   # basis definitions
   bas1B     = range(dim1B)
   bas2B     = construct_basis_2B(holes, particles)
-  basph2B   = construct_basis_ph2B(holes, particles)
+  #basph2B   = construct_basis_ph2B(holes, particles)
+  basph2B   = construct_basis_2B(holes, particles)
+
+  bas3B     = construct_basis_3B(holes, particles)
+  print "size of bas3B:", len(bas3B)
 
   idx2B     = construct_index_2B(bas2B)
   idxph2B   = construct_index_2B(basph2B)
+
+  idx3B     = construct_index_3B(bas3B)
 
   # occupation number matrices
   occ1B     = construct_occupation_1B(bas1B, holes, particles)
@@ -863,8 +1143,10 @@ def main():
     "bas1B":      bas1B,
     "bas2B":      bas2B,
     "basph2B":    basph2B,
+    "bas3B":      bas3B,
     "idx2B":      idx2B,
     "idxph2B":    idxph2B,
+    "idx3B":      idx3B,
     "occ1B":      occ1B,
     "occA_2B":    occA_2B,
     "occB_2B":    occB_2B,
@@ -875,8 +1157,10 @@ def main():
     "dE":         0.0,                # and main routine
 
 
-    "calc_eta":   eta_white,          # specify the generator (function object)
-    "calc_rhs":   flow_imsrg2         # specify the right-hand side and truncation
+#    "calc_eta":   eta_white,          # specify the generator (function object)
+#    "calc_rhs":   flow_imsrg2         # specify the right-hand side and truncation
+    "calc_eta":   eta_white_3B,       # specify the generator (function object)
+    "calc_rhs":   flow_imsrg3         # specify the right-hand side and truncation
   }
 
   # set up initial Hamiltonian
@@ -884,8 +1168,13 @@ def main():
 
   E, f, Gamma = normal_order(H1B, H2B, user_data) 
 
+  #SRS initial 3b term is zero
+  W = np.zeros((len(bas3B), len(bas3B)))
+
+
   # reshape Hamiltonian into a linear array (initial ODE vector)
-  y0   = np.append([E], np.append(reshape(f, -1), reshape(Gamma, -1)))
+#  y0   = np.append([E], np.append(reshape(f, -1), reshape(Gamma, -1)))
+  y0   = np.concatenate( ([E], reshape(f,-1), reshape(Gamma,-1), reshape(W,-1)) )
 
   # integrate flow equations 
   solver = ode(derivative_wrapper,jac=None)
@@ -896,25 +1185,29 @@ def main():
   sfinal = 50
   ds = 0.1
 
-  print "%-8s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s"%(
+  print "%-8s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s   %-14s"%(
     "s", "E" , "DE(2)", "DE(3)", "E+DE", "dE/ds", 
-    "||eta||", "||fod||", "||Gammaod||")
+    "||eta||", "||fod||", "||Gammaod||", "||Wod||", "||W||")
   # print "-----------------------------------------------------------------------------------------------------------------"
-  print "-" * 148
+  print "-" * (8+9*17)
   while solver.successful() and solver.t < sfinal:
     ys = solver.integrate(sfinal, step=True)
     
     dim2B = dim1B*dim1B
-    E, f, Gamma = get_operator_from_y(ys, dim1B, dim2B)
+    dim3B = dim1B*dim1B*dim1B
+#    E, f, Gamma = get_operator_from_y(ys, dim1B, dim2B)
+    E, f, Gamma, W = get_operator_from_y(ys, dim1B, dim2B, dim3B)
 
     DE2 = calc_mbpt2(f, Gamma, user_data)
     DE3 = calc_mbpt3(f, Gamma, user_data)
 
     norm_fod     = calc_fod_norm(f, user_data)
     norm_Gammaod = calc_Gammaod_norm(Gamma, user_data)
+    norm_Wod     = calc_Wod_norm(W, user_data)
+    norm_W       = np.linalg.norm(W)
 
-    print "%8.5f %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f"%(
-      solver.t, E , DE2, DE3, E+DE2+DE3, user_data["dE"], user_data["eta_norm"], norm_fod, norm_Gammaod)
+    print "%8.5f %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f   %14.8f"%(
+      solver.t, E , DE2, DE3, E+DE2+DE3, user_data["dE"], user_data["eta_norm"], norm_fod, norm_Gammaod, norm_Wod, norm_W )
     if abs(DE2/E) < 10e-8: break
 
 
